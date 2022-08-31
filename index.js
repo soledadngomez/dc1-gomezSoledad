56f890666edf69e018a7876ad10a5e9648f3be80
Soledad Noelia Gomez <95485411+soledadngomez@users.noreply.github.com>
2022-08-31 20:33:48 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+let unValor = prompt("Ingrese un número...(ESC para salir)")
+
+while (unNumero != "ESC"){
+    alert("El usuario ingresó:" + unNumero)
+    unNumero = prompt("Ingresa otro dato")
+
+}
