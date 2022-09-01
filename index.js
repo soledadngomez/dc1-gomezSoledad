d4c59599fa6bd6fec1299fb7fb4eb7da1e6c92f1
Soledad Noelia Gomez <95485411+soledadngomez@users.noreply.github.com>
2022-08-31 23:15:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-let unValor = prompt("Ingrese un número...(ESC para salir)")
+let bebida = prompt("Dame tu bebida");
+let comida = prompt("Dame tu comida");
 
-while (unNumero != "ESC"){
-    alert("El usuario ingresó:" + unNumero)
-    unNumero = prompt("Ingresa otro dato")
+let pedido = " vas a beber " + bebida + " y vas a comer " + comida;
 
+console.log("tu pedido es", pedido);
+
+let billete = prompt("son 26 pesos, dame tu billete");
+
+for (let i = 26; i <= billete; i++) {
+	console.log("billete", billete);
+	console.log("costo ", i);
+	if (i == billete) {
+		console.log("tu cambio son  " + billete - 26 + "tu billete era de " + billete);
+	}
 }
